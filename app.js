a85463fd1e325cc64698e344f9394758aefa027f
AshokBabuMannam <98141316+AshokBabuMannam@users.noreply.github.com>
2022-04-06 20:13:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var IcereamRouter = require('./routes/Icecream');
+var IcecreamRouter = require('./routes/icecream');
 
 var app = express();
 
